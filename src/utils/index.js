5389e17842f4a1c18769bd045581a0a5490a14cc
pritishvaidya <pritishvaidya94@gmail.com>
2019-02-15 00:43:56 +0530

    Adding the utils

---
+++ src/utils/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable*/
+const charFromUtf16 = utf16 => String.fromCodePoint(...utf16.split('-').map(u => `0x${u}`));
+
+function deepMerge(a, b) {
+  const o = {};
+
+  for (const key in a) {
+    const originalValue = a[key];
+
+
+    let value = originalValue;
+
+    if (b.hasOwnProperty(key)) {
+      value = b[key];
+    }
+
+    if (typeof value === 'object') {
+      value = deepMerge(originalValue, value);
+    }
+
+    o[key] = value;
+  }
+
+  return o;
+}
+
+export {
+  charFromUtf16,
+  deepMerge,
+};

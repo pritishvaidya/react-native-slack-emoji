34b182b99b9fc5860c652754c0cfdcfff41ed9d0
pritishvaidya <pritishvaidya94@gmail.com>
2019-02-19 00:34:27 +0530

    Adding the example 🔥

--- example/App.js
+++ example/App.js
@@ -6,35 +6,95 @@
 
 import React, { Component } from 'react';
 import {
-  Platform, StyleSheet, Text, View, TouchableOpacity,
+  StyleSheet, View, Text, Image,
 } from 'react-native';
-import { Picker } from 'react-native-slack-emoji';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Picker openPicker onPress={() => {}} />
-      </View>
-    );
-  }
-}
+import { Picker } from 'react-native-slack-emoji';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    padding: 10,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    paddingTop: 50,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  profileWrapper: {
+    flex: 1,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
+  image: {
+    height: 40,
+    resizeMode: 'contain',
+  },
+  name: {
+    fontSize: 16,
+    color: 'black',
+    fontWeight: '700',
     marginBottom: 5,
   },
+  text: {
+    fontSize: 15,
+    color: '#888888',
+    marginBottom: 10,
+  },
 });
+
+
+export default class App extends Component {
+  state = {
+    emojiList: [],
+  }
+
+  onSelect = (emoji, emojiName, data) => {
+    const { emojiList } = this.state;
+    const newList = [...emojiList];
+    const objIndex = newList.findIndex(e => e.name === emojiName);
+    if (objIndex === -1) {
+      newList.push({
+        emoji, name: emojiName, data, index: 1,
+      });
+    } else {
+      newList[objIndex].index += 1;
+    }
+    this.setState({ emojiList: newList });
+  }
+
+  updateEmoji = (emoji, name) => {
+    const { emojiList } = this.state;
+    const newList = [...emojiList];
+    const objIndex = newList.findIndex(e => e.name === name);
+    newList[objIndex].index += 1;
+    this.setState({ emojiList: newList });
+  }
+
+  render() {
+    const { emojiList } = this.state;
+    return (
+      <View style={styles.container}>
+        <View style={{ flex: 0.2 }}>
+          <Image source={{ uri: 'https://pritishvaidya.com/static/243ca309ea5cb3e20eb3c45bad183714/8cd81/favicon.png' }} style={styles.image} />
+        </View>
+        <View style={{ flex: 0.8 }}>
+          <Text style={styles.name}>Pritish Vaidya</Text>
+          <Text style={styles.text}>
+              We shall go on to the end. We shall fight in France,
+              we shall fight on the seas and oceans, we shall fight with growing confidence
+              and growing strength in the air, we shall defend our island, whatever
+              the cost may be.
+            {'\n'}
+            {'\n'}
+
+              We shall fight on the beaches, we shall fight
+              on the landing grounds, we shall fight in the fields and in the streets,
+              we shall fight in the hills; we shall never surrender
+          </Text>
+          <Picker
+            emojiList={emojiList}
+            updateEmoji={this.updateEmoji}
+            onSelect={this.onSelect}
+          />
+        </View>
+      </View>
+    );
+  }
+}
